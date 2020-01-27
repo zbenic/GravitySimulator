090ed350062ca73432237db6f83b082fb3337f6e
Jarvis Pennyworth <zbenic47250@gmail.com>
2020-01-27 23:17:13 +0100

    Added object initialisation. Implemented base class with position update.

--- gravitySimulation.py
+++ gravitySimulation.py
@@ -1,19 +1,41 @@
-import numpy
+import numpy as np
 import pygame
+from physicalObject import TerrestrialPlanet
 
 SIM_WINDOW_WIDTH = 800  # pixels
 SIM_WINDOW_HEIGHT = 600  # pixels
-MAX_OBJECT_DIAMETER = 100  # pixels
-MAX_NUM_OF_OBJECTS = 10
+MIN_OBJECT_DIAMETER = 5  # pixels
+MAX_OBJECT_DIAMETER = 50  # pixels
+MAX_NUM_OF_OBJECTS = 2
+FPS = 60
+TIMESTEP = 1. / FPS
+
 
 def init_objects():
-    random_float_array = numpy.random.uniform(75.5, 125.5, size=(2, 2))
+    # TODO: default class constructor and objects_array = [TerrestrialPlanet() for _ in range(MAX_NUM_OF_OBJECTS)]?
+    # TODO: watch it not to introduce collisions at the init phase
+    objects_array = []
+    position_vec = np.c_[np.random.randint(0, SIM_WINDOW_HEIGHT, MAX_NUM_OF_OBJECTS),
+                         np.random.randint(0, SIM_WINDOW_WIDTH, MAX_NUM_OF_OBJECTS)]
+    diameter = np.random.randint(MIN_OBJECT_DIAMETER, MAX_OBJECT_DIAMETER, MAX_NUM_OF_OBJECTS)
+    velocity_vec = np.zeros(MAX_NUM_OF_OBJECTS)
+    color = np.c_[np.random.randint(0, 255, MAX_NUM_OF_OBJECTS),
+                  np.random.randint(0, 255, MAX_NUM_OF_OBJECTS),
+                  np.random.randint(0, 255, MAX_NUM_OF_OBJECTS)]
     for i in range(0, MAX_NUM_OF_OBJECTS):
+        objects_array.append(TerrestrialPlanet(i,
+                                               position_vec[i],
+                                               diameter[i],
+                                               velocity_vec[i],
+                                               color[i],
+                                               MAX_NUM_OF_OBJECTS,
+                                               TIMESTEP))
+    return objects_array
 
 
-
-def draw_objects(thingx, thingy, thingw, thingh, color):
-    pygame.draw.rect(simDisplay, color, [thingx, thingy, thingw, thingh])
+def draw_objects(display, celestial_objects):
+    for celestial_object in celestial_objects:
+        pygame.draw.circle(display, celestial_object.color, celestial_object.position_vec, celestial_object.diameter)
 
 
 def main():
@@ -24,24 +46,21 @@ def main():
 
     clock = pygame.time.Clock()
 
+    celestial_objects = init_objects()
+
     crashed = False
     while not crashed:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 crashed = True
 
-            print(event)
+            draw_objects(simDisplay, celestial_objects)
 
         pygame.display.update()
         clock.tick(60)
 
     pygame.quit()
     quit()
-
-    #######
-    def things(thingx, thingy, thingw, thingh, color):
-        pygame.draw.rect(simDisplay, color, [thingx, thingy, thingw, thingh])
-    #######
 
 
 if __name__ == '__main__':
--- physicalObject.py
+++ physicalObject.py
@@ -3,39 +3,45 @@ import numpy as np
 GRAVITATIONAL_CONSTANT = 6.67408e-11  # m3 kg-1 s-2
 
 class PhysicalObject:
-    def __init__(self, object_id, position, diameter, velocity_vec, color):
+    def __init__(self, object_id, position, diameter, velocity_vec, color, total_object_count, timestep):
         self.object_id = object_id
         self.position_vec = position
         self.diameter = diameter
         self.velocity_vec = velocity_vec
         self.color = color
         self.mass = self.get_object_mass()
+        self.accelerations = np.zeros(shape=(total_object_count, 2))
+        self.timestep = timestep
 
     def get_object_mass(self):
-        raise NotImplementedError
+        return self.mass
 
-    def get_object_acc(self, celestial_objects):
+    def _get_object_acceleration(self, celestial_objects):
         #  http://www.scholarpedia.org/article/N-body_simulations_(gravitational), eq (2)
-        total_object_acc = 0.0
-        for obj_idx in range(0, len(celestial_objects)):
-            # paired_object = celestial_objects[obj_idx]
-            connecting_vec = np.subtract(self.position_vec, celestial_objects[obj_idx].position_vec)
-            total_object_acc += celestial_objects[obj_idx].mass * connecting_vec *\
-                (np.linalg.norm(connecting_vec)) ^ (-3)
+        total_object_acc = self.accelerations.sum(axis=0)
+        for celestial_object in celestial_objects:
+            connecting_vec = np.subtract(self.position_vec, celestial_object.position_vec)
+            celestial_object.accelerations[id] = celestial_object.mass * connecting_vec *\
+                                                 (np.linalg.norm(connecting_vec)) ^ (-3)
+            total_object_acc += celestial_object.accelerations[id]
         return -GRAVITATIONAL_CONSTANT * total_object_acc
 
-    def get_object_velocity(self):
-        object_acc_vec = self.get_object_acc()
-        return self.velocity_vec + object_acc_vec * dt
+    def _calculate_object_velocity(self, celestial_objects):
+        object_acc_vec = self._get_object_acceleration(celestial_objects)
+        self.velocity_vec += object_acc_vec * self.timestep
 
-    def get_object_next_position(self):
-        raise NotImplementedError
+    def _calculate_object_position(self):
+        self.position_vec += self.velocity_vec * self.timestep
+
+    def _get_object_position(self, celestial_objects):
+        self._calculate_object_velocity(celestial_objects)
+        self._calculate_object_position()
 
     def collide_object_with(self, collider_object):
-        return
+        raise NotImplementedError
 
-    def update(self):
-
+    def update(self, celestial_objects):
+        self._get_object_position(celestial_objects)
 
 
 class TerrestrialPlanet(PhysicalObject):
@@ -43,7 +49,7 @@ class TerrestrialPlanet(PhysicalObject):
     real_diameter = 12.742e6  # m
 
     def get_object_mass(self):
-            return self.density * 4 / 3 * np.pi * pow((self.diameter / 2), 3)
+        return self.density * 4 / 3 * np.pi * pow((self.diameter / 2), 3)
 
 
 class YellowSun(PhysicalObject):
